5c0f49fda0f9d28ccb962669128f5e602c058b39
Marcos111111 <106403146+Marcos111111@users.noreply.github.com>
2026-04-03 15:56:24 -0300

    Add new field 'Serrano' to CAMPOS list

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ CAMPOS = [
     {"nombre": "Elida", "lat": "-34.030491", "lon": "-63.202952"},
     {"nombre": "Magliano", "lat": "-34.243984", "lon": "-63.398968"},
     {"nombre": "Gomez", "lat": "-34.105827", "lon": "-63.429345"},
-    {"nombre": "Chañar", "lat": "-34.534439", "lon": "-63.618381"}
+    {"nombre": "Chañar", "lat": "-34.534439", "lon": "-63.618381"},
+    {"nombre": "Serrano", "lat": "-34.470862", "lon": "-63.538062"}
 ]
 
 TABLAS = ["recoleccion_yr", "recoleccion_mr", "recoleccion_ec"]
